7d719666fda61fd209c9926138e5837d70945a87
Barriguete Hector <50279990+BarrigueteHector@users.noreply.github.com>
2025-06-12 23:09:47 -0600

    Create program.js

---
+++ 6. Null & undefined values/program.js
@@ -0,0 +1,11 @@
+/* 
+null: represents an intentionally defined absence of value. 
+
+undefined: primitive value assigned to variables that have just been declared or to the resulting value of an operation that doesn't return a meaninful value. 
+
+null == undefined
+> true
+
+null === undefined
+> false
+*/
