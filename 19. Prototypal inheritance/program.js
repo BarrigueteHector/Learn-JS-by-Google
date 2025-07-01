b65139d6bb76147bccc58837230855a79f4c4b23
Barriguete Hector <50279990+BarrigueteHector@users.noreply.github.com>
2025-06-30 19:32:59 -0600

    Create program.js

---
+++ 19. Prototypal inheritance/program.js
@@ -0,0 +1,27 @@
+//Esta parte se debe de hacer en el navegador
+
+let myObject = {
+    'booleanValue': true
+}
+
+/* 
+myObject;
+
+> Object { booleanValue: true }
+    booleanValue: true
+    [[prototype]]: Object { … }
+            __defineGetter__: function __defineGetter__()
+            __defineSetter__: function __defineSetter__()
+            __lookupGetter__: function __lookupGetter__()
+            __lookupSetter__: function __lookupSetter__()
+            __proto__: …
+                constructor: function Object()
+                hasOwnProperty: function hasOwnProperty()
+                isPrototypeOf: function isPrototypeOf()
+                propertyIsEnumerable: function propertyIsEnumerable()
+                toLocaleString: function toLocaleString()
+                toString: function toString()
+                valueOf: function valueOf()
+                <get __proto__()>: function __proto__()
+                <set __proto__()>: function __proto__()
+*/
